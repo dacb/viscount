d37ceba9585e54f62961cad4384353ef333fb964
dacb <dacb@muffin.dhcp4.washington.edu>
2014-10-11 16:36:08 -0700

    pieces have been moved to respective subsytem files

--- viscount/forms.py
+++
@@ -1,7 +0,0 @@
-from flask.ext.wtf import Form
-from wtforms import form, fields, validators
-
-class LoginForm(Form):
-	username = fields.StringField('username', validators=[validators.required()])
-	password = fields.PasswordField('username', validators=[validators.required()])
-	remember_me = fields.BooleanField('remember_me', default=False)
