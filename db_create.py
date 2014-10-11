f81df36dc9156ccb2fcbb4a3d84bf55489154db0
dacb <dacb@muffin.dhcp4.washington.edu>
2014-10-11 16:35:55 -0700

    added example user, guest and project

--- db_create.py
+++ db_create.py
@@ -1,10 +1,15 @@
 #!venv/bin/python
+import os.path
+
 from migrate.versioning import api
+
 from config import SQLALCHEMY_DATABASE_URI
 from config import SQLALCHEMY_MIGRATE_REPO
+
 from viscount.server import db
 from viscount.auth import User
-import os.path
+from viscount.project import Project
+
 db.create_all()
 if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
     api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
@@ -13,6 +18,15 @@ else:
     api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, api.version(SQLALCHEMY_MIGRATE_REPO))
 
 # add default user
-user = User(username="admin", password="admin", active=True, role='admin')
+user = User(username='admin', password='admin', active=True, role='admin')
 db.session.add(user)
+user = User(username='user', password='user', active=True, role='user')
+db.session.add(user)
+user = User(username='guest', password='guest', active=True, role='guest')
+db.session.add(user)
+# create an example project
+project = Project(name='example', description='example project')
+db.session.add(project)
+
+# save the data
 db.session.commit()
