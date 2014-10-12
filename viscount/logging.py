e7aa214b948f08a3c75455ab0a1d954fc9de59ab
dacb <dacb@muffin.dhcp4.washington.edu>
2014-10-11 22:51:42 -0700

    added default timestamp to utcnow

--- viscount/logging.py
+++ viscount/logging.py
@@ -1,3 +1,4 @@
+import datetime
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask.ext.login import login_required
 from .server import app, db
@@ -6,7 +7,7 @@ from .auth import User
 class Log(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-	timestamp = db.Column(db.DateTime)
+	timestamp = db.Column(db.DateTime, default=datetime.datetime.utcnow())
 	project_id = db.Column(db.Integer, db.ForeignKey('project.id'))
 	file_id = db.Column(db.Integer, db.ForeignKey('file.id'))
 	type = db.Column(db.Enum('created', 'modified', 'deleted', 'accessed'))
