b55e485aa482a746d32223a2a06e2e26725056c7
dacb <dacb@muffin.dhcp4.washington.edu>
2014-10-13 15:35:46 -0700

    translating to flask-restful

--- viscount/views.py
+++ viscount/views.py
@@ -14,9 +14,3 @@ def index():
 @app.route("/robots.txt")
 def robotsTxt():
 	return send_from_directory(app.static_folder, "robots.txt")
-
-def register_api(view, endpoint, url, pk='id', pk_type='int'):
-	view_func = view.as_view(endpoint)
-	app.add_url_rule(url, defaults={pk: None}, view_func=view_func, methods=['GET',])
-	app.add_url_rule(url, view_func=view_func, methods=['POST',])
-	app.add_url_rule('%s<%s:%s>' % (url, pk_type, pk), view_func=view_func, methods=['GET', 'PUT', 'DELETE'])
