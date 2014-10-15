c919cae02803619ad48c44d637aedd1651aa9c9b
dacb <dacb@muffin.dhcp4.washington.edu>
2014-10-15 00:29:01 -0700

    renamed log to event + added event datatables func

--- viscount/event.py
+++ viscount/event.py
@@ -4,7 +4,7 @@ from flask.ext.login import login_required
 from .server import app, db
 from .user import User
 
-class Log(db.Model):
+class Event(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 	timestamp = db.Column(db.DateTime, default=datetime.datetime.utcnow())
@@ -15,7 +15,7 @@ class Log(db.Model):
 	type = db.Column(db.Enum('created', 'modified', 'deleted', 'accessed', 'login', 'logout', 'queued', 'started', 'finished', 'failed'), index=True)
 
 	def __repr__(self):
-		return '<Log %r>' % (self.id)
+		return '<Event %r>' % (self.id)
 
 	def message(self):
 		msg = []
@@ -33,7 +33,7 @@ class Log(db.Model):
 			msg.append('file %s' % self.file.filename)
 		return ' : '.join(msg)
 
-def logEntry(type, user=None, timestamp=datetime.datetime.utcnow(), project=None, file=None, job=None):
+def eventEntry(type, user=None, timestamp=datetime.datetime.utcnow(), project=None, file=None, job=None):
 	user_id = None
 	project_id = None
 	file_id = None
@@ -47,6 +47,22 @@ def logEntry(type, user=None, timestamp=datetime.datetime.utcnow(), project=None
 		file_id = file.id
 	if job is not None:
 		job_id = job.id
-	entry = Log(user_id=user_id, timestamp=timestamp, project_id=project_id, file_id=file_id, type=type, job_id=job_id, worker_id=worker_id)
+	entry = Event(user_id=user_id, timestamp=timestamp, project_id=project_id, file_id=file_id, type=type, job_id=job_id, worker_id=worker_id)
 	db.session.add(entry)
 	db.session.commit()
+
+@app.route('/events',  methods = ['GET', 'POST'])
+#@login_required
+def projects():
+	columns = []
+	columns.append(ColumnDT('id'))
+	columns.append(ColumnDT('user_id'))
+	columns.append(ColumnDT('timestamp'))
+	columns.append(ColumnDT('project_id'))
+	columns.append(ColumnDT('file_id'))
+	columns.append(ColumnDT('job_id'))
+	columns.append(ColumnDT('worker_id'))
+	columns.append(ColumnDT('type'))
+	query = db.session.query(Event)
+	rowTable = DataTables(request, Event, query, columns)
+	return jsonify(rowTable.output_result())
