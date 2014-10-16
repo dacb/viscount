0bac976863841aa5745c7d9a38681ab89b146fcf
dacb <dacb@muffin.dhcp4.washington.edu>
2014-10-16 01:56:53 -0700

    bug fixes

--- viscount/database.py
+++ viscount/database.py
@@ -9,7 +9,7 @@ def init_user(username, password, role):
 	from viscount.event import Event
 	user = User(username=username, password=password, role=role)
 	db.session.add(user)
-	db.session.add(Event(type='created', user=user)
+	db.session.add(Event(type='created', user=user))
 	return user
 
 def init_project(name, description, user):
@@ -21,14 +21,14 @@ def init_project(name, description, user):
 	return project
 
 def init_defaults():
-	init_user(username='admin', password='admin', role='admin'))
-	init_user(username='user', password='user', role='user'))
-	init_user(username='guest', password='guest', role='guest'))
+	init_user(username='admin', password='admin', role='admin')
+	init_user(username='user', password='user', role='user')
+	init_user(username='guest', password='guest', role='guest')
 	db.session.commit()
 
 def init_samples():
 	from viscount.user import User
 	admin = db.session.query(User).get(1)
 	for i in range(16):
-		init_project(name='sample' + str(i), description='sample project ' + str(i)), user=admin)
+		init_project(name='sample' + str(i), description='sample project ' + str(i), user=admin)
 	db.session.commit()
