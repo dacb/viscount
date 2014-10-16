e8086cbcc37039cd2533d728e626364bc81085bc
dacb <dacb@muffin.dhcp4.washington.edu>
2014-10-15 22:44:47 -0700

    basic manager for running, shell and datase

---
+++ viscount/manage/__init__.py
@@ -0,0 +1,19 @@
+import os.path
+from flask.ext.script import Manager, Server, Shell
+
+from viscount import app
+from viscount.database import db
+
+manager = Manager(app, usage="Manage viscount server installation")
+
+# add a default target
+manager.add_command("runserver", Server(host="0.0.0.0", port=5000))
+
+# add the shell target
+def _make_context():
+	return dict(app=app, db=db, models=models)
+manager.add_command("shell", Shell(make_context=_make_context))
+
+# include database manager
+from viscount.manage.database import manager as database_manager
+manager.add_command("database", database_manager)
---
+++ viscount/manage/database.py
@@ -0,0 +1,82 @@
+#!venv/bin/python
+import os.path
+from migrate.versioning import api
+from flask.ext.script import Manager, prompt_bool
+
+from config import SQLALCHEMY_DATABASE_URI
+from config import SQLALCHEMY_MIGRATE_REPO
+
+from viscount import app
+from viscount.database import db
+
+manager = Manager(usage="Perform database operations")
+
+@manager.command
+def drop():
+	"Drops database tables"
+	if prompt_bool("Are you sure you want to lose all your data"):
+		db.drop_all()
+
+@manager.command
+def create(default_data=True, sample_data=False):
+	"Creates database tables from sqlalchemy models"
+	db.create_all()
+	if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
+		api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
+		api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+	else:
+		api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, api.version(SQLALCHEMY_MIGRATE_REPO))
+	populate(default_data, sample_data)
+
+
+@manager.command
+def recreate(default_data=True, sample_data=False):
+	"Recreates database tables (same as issuing 'drop' and then 'create')"
+	drop()
+	create(default_data, sample_data)
+
+
+@manager.command
+def populate(default_data=False, sample_data=False):
+	"Populate database with default data"
+
+	if default_data:
+		from viscount.database import init_default
+		init_default()
+
+	if sample_data:
+		from viscount.database import init_sample
+		init_sample()
+
+@manager.command
+def migrate():
+	"Migrate data model"
+
+	v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+	migration = SQLALCHEMY_MIGRATE_REPO + ('/versions/%03d_migration.py' % (v+1))
+	tmp_module = imp.new_module('old_model')
+	old_model = api.create_model(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+	exec(old_model, tmp_module.__dict__)
+	script = api.make_update_script_for_model(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, tmp_module.meta, db.metadata)
+	open(migration, "wt").write(script)
+	api.upgrade(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+	v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+	print('New migration saved as ' + migration)
+	print('Current database version: ' + str(v))
+
+@manager.command
+def upgrade():
+	"Upgrade data model"
+
+	api.upgrade(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+	v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+	print('Current database version: ' + str(v))
+
+@manager.command
+def downgrade():
+	"Downgrade data model"
+
+	v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+	api.downgrade(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, v - 1)
+	v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+	print('Current database version: ' + str(v))
