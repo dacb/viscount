bb229b50a12fe74bfb9b11e3ee981ba880dea034
dacb <dacb@muffin.dhcp4.washington.edu>
2014-10-16 00:45:37 -0700

    intermediate checkin

--- viscount/test/stub.py
+++ viscount/test/stub.py
@@ -1,18 +1,18 @@
-#!venv/bin/python
 import unittest
+import os.path
 
 from viscount import app
-from viscount.database import db, init_defaults, init_sample
+from viscount.database import db, init_defaults, init_samples
 
 class SuiteTests(unittest.TestCase):
 	def setUp(self):
 		app.config['TESTING'] = True
 		app.config['WTF_CSRF_ENABLED'] = False
-		app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db')
+		app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.config['DATADIR'], 'test.db')
 		self.app = app.test_client()
 		db.create_all()
 		init_defaults()
-		init_sample()
+		init_samples()
 
 	def tearDown(self):
 		db.session.remove()
