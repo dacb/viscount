595c40b1caf78a998d091c03efec587f606cec48
Dave Beck <dacb@uw.edu>
2014-10-17 00:15:46 -0700

    continue on failed is no longer the default
    the user can supply the -c (continue) flag to force the unittests
    to continue on a failed or error condition
    
    when testing a specific problem, this can be useful to get results quickly
    but -c runs should be performed regularly to detect regressions

--- viscount/test/__init__.py
+++ viscount/test/__init__.py
@@ -1,10 +1,24 @@
 from flask.ext.script import Manager, Server, Shell
 import unittest
 
+#
 # in accordance with the following, tests should be named:
 # test_HelpfulTestName_X_Y
 # where X and Y are numbers, tests will be sorted by X first and if equal then Y
 unittest.TestLoader.sortTestMethodsUsing = lambda _, x, y: cmp(int(x.rsplit('_', 2)[1]), int(y.rsplit('_', 2)[1])) if cmp(int(x.rsplit('_', 2)[1]), int(y.rsplit('_', 2)[1])) != 0 else cmp(int(x.rsplit('_', 1)[1]), int(y.rsplit('_', 1)[1]));
+#
+
+# 
+# this overides the default unittest runner to stop on any previous failures or errors
+# note the abort happens on the run of the next failed test case
+def aborting_run(self, result=None):
+    if result.failures or result.errors:
+        print "aborted"
+    else:
+        original_run(self, result)
+original_run = unittest.TestCase.run
+unittest.TestCase.run = aborting_run
+#
 
 from viscount import app
 from viscount.database import db
@@ -12,8 +26,11 @@ from viscount.database import db
 manager = Manager(app, usage="Test harness for viscount", with_default_commands=False)
 
 @manager.command
-def run(db=False, stub=False, loginout=False, tables=False, all=True):
+def run(db=False, stub=False, loginout=False, tables=False, all=True, continueOnFail=False):
 	suite = unittest.TestSuite()
+
+	if continueOnFail:
+		unittest.TestCase.run = original_run
 
 	if db or stub or loginout or tables:
 		all = False
