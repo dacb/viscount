5149cff6803109ef8cab750d8bff2473ce94773d
dacb <dacb@muffin.dhcp4.washington.edu>
2014-10-16 22:43:35 -0700

    switched from random key each startup to keep sessions

--- config.py
+++ config.py
@@ -4,7 +4,11 @@ BASEDIR = os.path.abspath(os.path.dirname(__file__))
 DATADIR = os.path.join(BASEDIR, 'data')
 
 WTF_CSRF_ENABLED = True
-SECRET_KEY = os.urandom(24)
+# generate your own key, e.g.
+# import os
+# os.urandom(24)
+# copy it and use it here
+SECRET_KEY = 'c\xde\xb0\xc2S\x0b\x88\xbbN\x05\xf0My#\x8a\t%1\xd2\xc0.\xcf\x1c\x11'
 
 SQLALCHEMY_DATABASE_URI = 'mysql://viscount:viscountRocks@localhost/viscount'
 #SQLALCHEMY_ECHO = True
