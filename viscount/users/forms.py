6acdbc5154d2063e3be797d43bb89c1ad54b6a0d
dacb <dacb@uw.edu>
2014-10-26 13:52:47 -0700

    added requsite fields to complete form

--- viscount/users/forms.py
+++ viscount/users/forms.py
@@ -6,7 +6,7 @@ Note: these are handled by flask-security and exist just for the API
 
 from flask_wtf import Form
 from wtforms import StringField, TextAreaField
-from wtforms.validators import Required, Optional
+from wtforms.validators import Required, Optional, Email, AnyOf
 
 from ..services import users
 
@@ -14,13 +14,19 @@ __all__ = ['NewUserForm', 'UpdateUserForm']
 
 
 class NewUserForm(Form):
+	email = StringField('email', validators=[Required(), Email()])
 	username = StringField('username', validators=[Required()])
+	password = StringField('password', validators=[Required()])
+	roles = [StringField('role', validators=[Required(), AnyOf(values=['admin', 'user', 'guest'])])]
 	firstName = StringField('firstName', validators=[Required()])
 	lastName = StringField('lastName', validators=[Required()])
 
 
 class UpdateUserForm(Form):
-	username = StringField('username', validators=[Optional()])
+	email = StringField('email', validators=[Required(), Email()])
+	username = StringField('username', validators=[Required()])
+	password = StringField('password', validators=[Required()])
+	roles = [StringField('role', validators=[Required(), AnyOf(values=['admin', 'user', 'guest'])])]
 	firstName = StringField('firstName', validators=[Required()])
 	lastName = StringField('lastName', validators=[Required()])
 
