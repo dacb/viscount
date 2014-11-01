ca1dfbc9dc291d69fc87ce22504e5f5bcedabd54
Dave Beck <dacb@uw.edu>
2014-11-01 01:42:50 -0700

    checkpoint of role checkboxes in create user form

--- viscount/users/forms.py
+++ viscount/users/forms.py
@@ -5,8 +5,8 @@ Note: these are handled by flask-security and exist just for the API
 """
 
 from flask_wtf import Form
-from wtforms import StringField, TextAreaField
-from wtforms.validators import Required, Optional, Email, AnyOf
+from wtforms import StringField, TextAreaField, SelectMultipleField
+from wtforms.validators import Required, Email
 
 from ..services import users
 
@@ -17,16 +17,15 @@ class NewUserForm(Form):
 	email = StringField('email', validators=[Required(), Email()])
 	username = StringField('username', validators=[Required()])
 	password = StringField('password', validators=[Required()])
-	roles = [StringField('role', validators=[Required(), AnyOf(values=['admin', 'user', 'guest'])])]
 	firstName = StringField('firstName', validators=[Required()])
 	lastName = StringField('lastName', validators=[Required()])
+	roles = SelectMultipleField('roles', choices=[('admin', '1'), ('user', '2'), ('guest', '3')])
 
 
 class UpdateUserForm(Form):
 	email = StringField('email', validators=[Required(), Email()])
 	username = StringField('username', validators=[Required()])
 	password = StringField('password', validators=[Required()])
-	roles = [StringField('role', validators=[Required(), AnyOf(values=['admin', 'user', 'guest'])])]
 	firstName = StringField('firstName', validators=[Required()])
 	lastName = StringField('lastName', validators=[Required()])
 
