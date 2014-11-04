e044c871ef4a552db774d30e306057ca0fc55531
Dave Beck <dacb@uw.edu>
2014-11-03 21:02:00 -0800

    renamed model field to reflect reality

--- viscount/files/models.py
+++ viscount/files/models.py
@@ -17,7 +17,7 @@ class File(FileJSONSerializer, db.Model):
 	__tablename__ = 'files'
 
 	id = db.Column(db.Integer, primary_key=True)
-	filename = db.Column(db.String(255), index=False, unique=False)
+	name = db.Column(db.String(255), index=False, unique=False)
 	user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 	description = db.Column(db.Text, index=False, unique=False)
 	md5sum = db.Column(db.String(32), index=True, unique=False)
