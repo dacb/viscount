b92e21e59b82b268bb62d00dc151123a373aaa49
Dave Beck <dacb@uw.edu>
2014-11-04 20:50:46 -0800

    added file create method

--- viscount/files/__init__.py
+++ viscount/files/__init__.py
@@ -4,8 +4,30 @@ viscount.files
 provides file related services
 """
 
+from flask_security.core import current_user
+
+
 from ..core import Service
 from .models import File
 
 class FilesService(Service):
 	__model__ = File
+
+	def create(self, **kwargs):
+		"""Returns a new, saved instance of the file model class.
+
+		:param **kwargs: instance parameters
+		"""
+		from ..services import events
+		file = self.save(self.new(**kwargs))
+		event = events.create(file_id=file.id, user_id=current_user.id, type='created')
+		return file
+
+	def new(self, **kwargs):
+		"""Returns a new, unsaved instance of the service's model class.
+
+		:param **kwargs: instance parameters
+		"""
+		kwargs.update({'user_id' : current_user.id })
+		return self.__model__(**self._preprocess_params(kwargs))
+
