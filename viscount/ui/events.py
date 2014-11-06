793b7949f67435419186e308976fd8348a4f5410
dacb <beck.dac@live.com>
2014-11-06 15:57:40 -0800

    added workflow column

--- viscount/ui/events.py
+++ viscount/ui/events.py
@@ -2,7 +2,7 @@ from flask.ext.security.core import current_user
 from flask import request, jsonify
 
 from . import route
-from ..models import Event, User, Project, File
+from ..models import Event, User, Project, File, Workflow
 from ..core import db
 from .datatables import DataTables
 from .index import bp
@@ -19,6 +19,8 @@ def events():
 		'project.name' : True,
 		'file_id' : True,
 		'file.filename' : True,
+		'workflow_id' : True,
+		'workflow.filename' : True,
 		'job_id' : True,
 		'worker_id' : True,
 		'type' : True,
@@ -26,6 +28,7 @@ def events():
 	query = db.session.query(Event). \
 		outerjoin(User, (User.id == Event.user_id)). \
 		outerjoin(Project, (Project.id == Event.project_id)). \
-		outerjoin(File, (File.id == Event.file_id))
+		outerjoin(File, (File.id == Event.file_id)). \
+		outerjoin(Workflow, (Workflow.id == Event.workflow_id))
 	rowTable = DataTables(request, Event, query)
 	return jsonify(rowTable.output_result())
